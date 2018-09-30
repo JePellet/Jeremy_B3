75d17eb3bde7d2a2d5f0d80465df7d6d12c7bf08
JePellet <43410916+JePellet@users.noreply.github.com>
2018-09-30 15:36:05 +0200

    update 30/09

---
+++ API Bicloo/js/main.js
@@ -0,0 +1,37 @@
+fetch('http://api.citybik.es/v2/networks/bicloo').then(function(res) {
+	return res.json().then(function(data) {
+    var stations = data.network.stations;
+
+    //Récupération de la balise html dans laquelle on va bosser
+    liste = document.querySelector('ul');
+
+    for (i = 0; i < stations.length; i++) {
+      //Calcul de la proportion de vélos dispo
+      var proportion = (stations[i].free_bikes/stations[i].extra.slots);
+
+			//Mise en avant si peu ou pas de vélos dispo
+			if (proportion < 0.5) {
+      	currentLi.classList.add('prop');
+      } else if (proportion === 0) {
+      	currentLi.classList.add('propZero');
+      }
+
+      //Création des balises à ajouter au html
+      var currentLi = document.createElement('li');
+			var currentBr = document.createElement('br');
+
+      //Ecriture du contenu de la puce
+    	currentLi.textContent = stations[i].name +
+      ' // Places libres : ' +
+      stations[i].empty_slots +
+      ' // Vélo(s) libre(s) : ' +
+      stations[i].free_bikes
+      ;
+
+      //Ajout de la puce et du passage à la ligne à la page html
+    	liste.appendChild(currentLi);
+			liste.appendChild(currentBr);
+    }
+
+	});
+});
