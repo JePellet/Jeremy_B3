9823e2dd849f7fedd431d2ca455f1e8764ecef93
JePellet <43410916+JePellet@users.noreply.github.com>
2018-10-02 23:44:35 +0200

    /0 évitée, 0 vélo réussi

--- API Bicloo/js/main.js
+++ API Bicloo/js/main.js
@@ -1,49 +1,36 @@
 fetch('http://api.citybik.es/v2/networks/bicloo').then(function(res) {
 	return res.json().then(function(data) {
-
     var stations = data.network.stations;
 
     //Récupération de la balise html dans laquelle on va bosser
-    liste = document.querySelector('ul');
+    liste = document.querySelector('div');
 
     for (i = 0; i < stations.length; i++) {
       //Calcul de la proportion de vélos dispo
-      var proportion = (stations[i].free_bikes/stations[i].extra.slots);
+			// En évitant la division par 0
+			if (stations[i].extra.slots !== 0) {
+					var proportion = (stations[i].free_bikes/stations[i].extra.slots);
+			}
 
-      //Création des balises à ajouter au html
-      var currentLi = document.createElement('li');
-			var currentBr = document.createElement('br');
-
-			//Mise en avant si peu ou pas de vélos dispo
-			if (proportion < 0.5) {
-      	currentLi.classList.add('prop');
-
-				//Ecriture du contenu de la puce
-				currentLi.textContent = stations[i].name +
-				' // Places libres : ' +
-				stations[i].empty_slots +
-				' // Vélo(s) libre(s) : ' +
-				stations[i].free_bikes;
+			//Création des balises à ajouter au html
+      var currentDiv = document.createElement('div');
+			currentDiv.classList.add('display');
 
 
-			//LA 2E CONDITION NE MARCHE PAS : free_bikes === 0
-		} else if ((stations[i].extra.status === "CLOSED") || (stations[i].free_bikes === 0)) {
-      	currentLi.classList.add('noBikes');
+			//Assignation de classes pour les stations qui ont moins de 50% des vélos dispo
+      if (stations[i].free_bikes === 0) {
+      	currentDiv.classList.add('noBikes');
+      } else if (proportion < 0.5) {
+      	currentDiv.classList.add('prop');
+			}
 
-				//Ecriture du contenu de la puce
-				currentLi.textContent = stations[i].name +
-				'		EST FERMEE'
-      } else {
-				currentLi.textContent = stations[i].name +
-	      ' // Places libres : ' +
-	      stations[i].empty_slots +
-	      ' // Vélo(s) libre(s) : ' +
-	      stations[i].free_bikes;
-      }
+      //Ecriture du contenu du div
+    	currentDiv.textContent = stations[i].name +
+      ' // Places libres : ' + stations[i].empty_slots +
+      ' // Vélo(s) libre(s) : ' + stations[i].free_bikes;
 
       //Ajout de la puce et du passage à la ligne à la page html
-    	liste.appendChild(currentLi);
-			liste.appendChild(currentBr);
+    	liste.appendChild(currentDiv);
     }
 
 	});
