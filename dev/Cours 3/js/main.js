f71241fda48026cd6628ce5a8f5240717ca3124c
JePellet <43410916+JePellet@users.noreply.github.com>
2018-10-03 19:12:47 +0200

    pleins de trucs

---
+++ dev/Cours 3/js/main.js
@@ -0,0 +1,22 @@
+var myDiv = document.querySelector(".clickMe");
+
+myDiv.addEventListener('click', function() {
+  console.log("test");
+});
+
+var threshold = 400;
+var header = document.querySelector("header");
+
+window.addEventListener("scroll", function(event) {
+  if (window.scrollY > threshold) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+var myBurger = document.querySelector('.burger');
+
+myBurger.addEventListener("click", function() {
+  document.querySelector('nav').classList.toggle('visible');
+});
